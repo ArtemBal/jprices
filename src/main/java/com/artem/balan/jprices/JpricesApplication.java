1a26189b648dd29b5d8d890f26ebc55b7915bea7
Artem Balan <a.s.balan96@gmail.com>
2024-04-19 21:53:52 +0300

    init commit

---
+++ src/main/java/com/artem/balan/jprices/JpricesApplication.java
@@ -0,0 +1,13 @@
+package com.artem.balan.jprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artem/balan/jprices/JpricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artem.balan.jprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
