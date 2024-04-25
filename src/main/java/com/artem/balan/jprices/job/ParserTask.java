2a2d3bc9a56d0ca9c3e179572399f6d292bb80e2
Artem Balan <a.s.balan96@gmail.com>
2024-04-25 23:55:31 +0300

    Fix annotations

--- src/main/java/com/artem/balan/jprices/job/ParserTask.java
+++ src/main/java/com/artem/balan/jprices/job/ParserTask.java
@@ -18,7 +18,7 @@ public class ParserTask {
     @Autowired
     private PriceService priceService;
 
-    @Scheduled(fixedDelay = 50_000_0) // 500s
+    @Scheduled(fixedDelay = 20_000) // 20s
     public void parsePrices() {
         String siteUrlA = "https://agrotender.com.ua/traders/region_vinnica/soya";
         String siteUrlB = "https://tripoli.land/ua/analytics/soya";
@@ -33,12 +33,18 @@ public class ParserTask {
 
     // agrotender.com.ua
     public void parsePricesA(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Element table = doc.select("table").get(0).select("tbody").get(0);
+        Document doc = Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " +
+                        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36")
+                .referrer("http://www.google.com")
+                .get();
+        Element table = doc.select("table")
+                .get(0)
+                .select("tbody")
+                .get(0);
         Elements trs = table.select("tr");
         for (Element row : trs) {
             Elements tds = row.select("td");
-
             Offer offer = new Offer();
             offer.setCrop("соя");
             offer.setPrice(tds.get(1).text());
@@ -49,8 +55,15 @@ public class ParserTask {
 
     // tripoli.land
     public void parsePricesB(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Element table = doc.select("table").get(0).select("tbody").get(0);
+        Document doc = Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " +
+                        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36")
+                .referrer("http://www.google.com")
+                .get();
+        Element table = doc.select("table")
+                .get(0)
+                .select("tbody")
+                .get(0);
         Elements trs = table.select("tr");
         Offer offer = new Offer();
         offer.setCrop("соя");
--- src/main/java/com/artem/balan/jprices/service/PriceServiceImpl.java
+++ src/main/java/com/artem/balan/jprices/service/PriceServiceImpl.java
@@ -3,9 +3,11 @@ package com.artem.balan.jprices.service;
 import com.artem.balan.jprices.model.Offer;
 import com.artem.balan.jprices.repository.PriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PriceServiceImpl implements PriceService {
 
     @Autowired
